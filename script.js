5159e177a270ee17b25c994430ea1b686c0536c3
anuraggiri4308 <anuraggiri4308@gmail.com>
2022-03-25 13:40:55 +0530

    master

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+const input = document.querySelector(".text-input");
+const btn = document.querySelector(".btn");
+const msg = document.querySelector(".message");
+let filterInput;
+btn.addEventListener("click", () => {
+  // splitting user input and then joining them in a single unit
+  let reverseInput = filterInput.split("").reverse().join("");
+  if (filterInput != reverseInput) {
+    msg.classList.add("error");
+
+    msg.style.display = "block";
+    msg.textContent = `${filterInput} is not a Palindrome`;
+    return (msg.innerHTML = `Your input <span class="user-input">"${input.value}"</span> is not a Palindrome number.`);
+  }
+  msg.style.display = "block";
+  msg.classList.remove("error");
+  msg.classList.add("success");
+  return (msg.innerHTML = `Your input <span class="user-input">"${input.value}"</span> is a Palindrome number.`);
+});
+
+input.addEventListener("keyup", () => {
+  // remove space and special character
+  filterInput = input.value.toLowerCase().replace(/[^A-Z0-9]/gi, "");
+  if (filterInput) {
+    return btn.classList.add("active");
+  }
+  msg.style.display = "none";
+  btn.classList.remove("active");
+});
